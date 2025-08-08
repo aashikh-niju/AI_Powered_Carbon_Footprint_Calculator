70ecfa23a3bb213b7fe6ad19821b39fe970aeb3e
aashikh-niju <149099113+aashikh-niju@users.noreply.github.com>
2025-08-08 22:44:31 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI_Powered_Carbon_Footprint_Calculator/', // 👈 IMPORTANT
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
